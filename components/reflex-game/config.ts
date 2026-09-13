3ef4f4bde265cb321840b9911b67868283892f45
TokyoS-code <hesmuelfcezar@gmail.com>
2026-09-13 18:48:34 -0300

    Update config.ts

--- components/reflex-game/config.ts
+++ components/reflex-game/config.ts
@@ -2,15 +2,15 @@ export const GAME_CONFIG = {
   blockDurationMs: 700,
   totalCooldownMs: 1000,
 
-  soloInitialSpeed: 500,
+  soloInitialSpeed: 900,
   multiplayerInitialSpeed: 430,
 
-  soloSpeedIncrease: 0.08,
+  soloSpeedIncrease: 0.37,
   multiplayerSpeedIncrease: 0.035,
 
   returnSpeedMultiplier: 1.35,
 
-  minWaitMs: 700,
+  minWaitMs: 600,
   maxWaitMs: 3000,
   multiplayerStartDelayMs: 900,
 
